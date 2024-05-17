e5c552767756897d01caef73cf97160719f7a9c0
Render78 <mariojuliantor@hotmail.com>
2024-05-17 14:35:56 -0300

    express server done

---
+++ primera-practica/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
