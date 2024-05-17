f6b7d096c68e1d1333e3b5a76c030791d3087f9b
Render78 <mariojuliantor@hotmail.com>
2024-05-17 15:42:07 -0300

    crud endpoints done

---
+++ primera-practica/src/routes/products.router.js
@@ -0,0 +1,48 @@
+import { Router } from "express";
+import productsModel from "../dao/models/products.model.js";
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+    try {
+        let products = await productsModel.find();
+        res.send({ result: "success", payload: products });
+    } catch (error) {
+        console.error("No se pudieron obtener los productos", error);
+    }
+})
+
+router.post("/", async (req, res) => {
+    try {
+        let { title, description, price, thumbnail, code, stock, status } = req.body;
+        if (!title, !description, !price, !thumbnail, !code, !stock, !status) {
+            res.send({ status: "error", error: "Algunos parametros estan vacios" });
+        }
+        let result = await productsModel.create({ title, description, price, thumbnail, code, stock, status });
+        res.send({ result: "success", payload: result });
+    } catch (error) {
+        console.error("No se pudo agregar el producto", error);
+    }
+})
+
+router.put("/:pid", async (req, res) => {
+    let { pid } = req.params;
+
+    let productToReplace = req.body;
+
+    if (!productToReplace.title || !productToReplace.description || !productToReplace.price || !productToReplace.thumbnail || !productToReplace.code || !productToReplace.stock || !productToReplace.status) {
+        res.send({ status: "error", error: "Algunos parametros estan vacios" });
+    }
+
+    let result = await productsModel.updateOne({ _id: pid }, productToReplace);
+
+    res.send({ result: "success", payload: result });
+})
+
+router.delete("/:pid", async (req, res) => {
+    let { pid } = req.params;
+    let result = await productsModel.deleteOne({ _id: pid });
+    res.send({result: "success", payload: result});
+})
+
+export default router;
