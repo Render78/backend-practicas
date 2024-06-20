8a5a77dc611113ddd74ee5d49d2a64e1bd253d56
Render78 <mariojuliantor@hotmail.com>
2024-06-20 13:49:05 -0300

    fixing some errors

--- segunda-practica/src/app.js
+++ segunda-practica/src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import mongoose from './config/database.js';
 import handlebars from 'express-handlebars';
+import dotenv from 'dotenv';
+import MongoStore from 'connect-mongo';
+import passport from 'passport';
+import initializePassport from './config/passport.config.js';
 import __dirname from './utils.js';
-import dotenv from 'dotenv'
-import indexRouter from './routes/index.router.js';
+import sessionsRouter from './routes/api/sessions.router.js';
+import viewsRouter from './routes/views.router.js';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
 
@@ -13,6 +19,9 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 dotenv.config();
 app.use(session({
     secret: 'secretkey',
@@ -20,19 +29,20 @@ app.use(session({
     saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
 }));
-// mongoose.connect(process.env.MONGO_URL)
-//     .then(() => { console.log("Conectado a la base de datos") })
-//     .catch(error => console.error("Error en la conexion", error))
 
+initializePassport()
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.engine('handlebars', handlebars.engine())
 app.set('views', __dirname + '/views')
 app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
 
-app.use('/', indexRouter);
-app.use('/api/products', productsRouter)
-app.use('/api/carts', cartsRouter)
+app.use('/', viewsRouter);
+app.use('/api/sessions', sessionsRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- segunda-practica/src/config/passport.config.js
+++ segunda-practica/src/config/passport.config.js
@@ -1,90 +1,96 @@
 import passport from "passport";
-import { Strategy as LocalStrategy } from 'passport-local'
+import local from 'passport-local'
 import { Strategy as GitHubStrategy } from 'passport-github2';
 import userModel from '../dao/models/user.model.js';
 import { createHash } from "../utils.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
-passport.use('register', new LocalStrategy(
-    { passReqToCallback: true, usernameField: 'email' },
-    async (req, email, password, done) => {
-        const { first_name, last_name, age } = req.body;
-        try {
-            let user = await userModel.findOne({ email });
-            if (user) {
-                console.log("El usuario ya existe");
-                return done(null, false);
-            }
-            const newUser = new userModel({
-                first_name,
-                last_name,
-                email,
-                age,
-                password: createHash(password)
-            });
-            let result = await newUser.save();
-            return done(null, result);
-        } catch (error) {
-            return done(error);
-        }
-    }
-));
+const LocalStrategy = local.Strategy;
 
-passport.use('login', new LocalStrategy(
-    { usernameField: 'email' },
-    async (email, password, done) => {
-        try {
-            const user = await userModel.findOne({ email });
-            if (!user) {
-                console.log("El usuario no existe");
-                return done(null, false);
-            }
-            if (!user.comparePassword(password)) {
-                console.log("Contraseña incorrecta");
-                return done(null, false);
-            }
-            return done(null, user);
-        } catch (error) {
-            return done(error);
-        }
-    }
-));
-
-passport.use('github', new GitHubStrategy(
-    {
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "http://localhost:8080/api/sessions/githubcallback"
-    },
-    async (accessToken, refreshToken, profile, done) => {
-        try {
-            let user = await userModel.findOne({ email: profile._json.email });
-            if (!user) {
+const initializePassport = () => {
+    passport.use('register', new LocalStrategy(
+        { passReqToCallback: true, usernameField: 'email' },
+        async (req, email, password, done) => {
+            const { first_name, last_name, age } = req.body;
+            try {
+                let user = await userModel.findOne({ email });
+                if (user) {
+                    console.log("El usuario ya existe");
+                    return done(null, false);
+                }
                 const newUser = new userModel({
-                    first_name: profile._json.name,
-                    email: profile._json.email,
-                    age: 20,
-                    password: ""
+                    first_name,
+                    last_name,
+                    email,
+                    age,
+                    password: createHash(password)
                 });
-                user = await newUser.save();
+                let result = await newUser.save();
+                return done(null, result);
+            } catch (error) {
+                return done(error);
             }
-            return done(null, user);
-        } catch (error) {
-            return done(error);
         }
-    }
-));
+    ));
 
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
+    passport.use('login', new LocalStrategy(
+        { usernameField: 'email' },
+        async (email, password, done) => {
+            try {
+                const user = await userModel.findOne({ email });
+                if (!user) {
+                    console.log("El usuario no existe");
+                    return done(null, false);
+                }
+                if (!user.comparePassword(password)) {
+                    console.log("Contraseña incorrecta");
+                    return done(null, false);
+                }
+                return done(null, user);
+            } catch (error) {
+                return done(error);
+            }
+        }
+    ));
 
-passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await userModel.findById(id);
-        done(null, user);
-    } catch (error) {
-        done(error);
-    }
-});
+    passport.use('github', new GitHubStrategy(
+        {
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: "http://localhost:8080/api/sessions/githubcallback"
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            try {
+                let user = await userModel.findOne({ email: profile._json.email });
+                if (!user) {
+                    const newUser = new userModel({
+                        first_name: profile._json.name,
+                        email: profile._json.email,
+                        age: 20,
+                        password: ""
+                    });
+                    user = await newUser.save();
+                }
+                return done(null, user);
+            } catch (error) {
+                return done(error);
+            }
+        }
+    ));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await userModel.findById(id);
+            done(null, user);
+        } catch (error) {
+            done(error);
+        }
+    });
+}
+
+export default initializePassport
